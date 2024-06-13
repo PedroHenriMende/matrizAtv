809e344b245edd4e1266922a9f2368315fcba2eb
PedroHenriMende <161759272+PedroHenriMende@users.noreply.github.com>
2024-06-13 11:35:29 -0300

    Update atividadeQuatro.js

--- atividadeQuatro.js
+++ atividadeQuatro.js
@@ -1,20 +1,19 @@
 /*Pedro Henrique Mendes De Jesus
-3- Altere o código um(1) desse conteúdo para funcionar com números e ter duas colunas. */
+4- Altere o código dois(2) desse conteúdo para funcionar
+com números, faça o teste se o laço de repetição
+referente a linha da matriz chega criar a quarta linha, salve
+dessa forma e anexe a atividade o arquivo .ts e um print
+da tela do cmd(prompt command) com o resultado do
+teste. */
 console.clear();
 var teclado = require("prompt-sync")();
-var minhaMatriz = []; //declarando Matriz //
-var linha = 1; //variavel para controlar linha //
-var coluna = 1; //variavel para controlar coluna //
-// z percorre as linhas  
-for (var z = 0; z <= linha; z++) {
-    // Cria um array vazio para preencher a linha correspondente ao z  
-    minhaMatriz[z] = [];
-    for (var x = 0; x <= coluna; x++) {
-        // Atribuicaao via entrada de dados manual na variavel nome  
-        var nome = teclado("Digite o nome que vai estar no endereco [".concat(z, ", ").concat(x, "]  sda matriz: ;"));
-        // Atribui nome na minhaMatriz do endereço dado por z e x  
-        minhaMatriz[z][x] = nome;
+var minhaMatriz = [];
+for (var x = 0; x < 3; x++) {
+    minhaMatriz[x] = new Array(3);
+    for (var i = 0; i < 4; i++) {
+        var nome = teclado("Digite os numeros no [".concat(x, ", ").concat(i, "]"));
+        minhaMatriz[x][i] = nome;
     }
 }
-// Imprime a matriz  
 console.log(minhaMatriz);
+
