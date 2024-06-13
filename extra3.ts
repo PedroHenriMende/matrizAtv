116faadc5737db6e124c7e63a8cf60d3b5b845aa
PedroHenriMende <161759272+PedroHenriMende@users.noreply.github.com>
2024-06-13 10:57:11 -0300

    Update extra3.ts

--- extra3.ts
+++ extra3.ts
@@ -4,3 +4,30 @@ em um array, e imprime o maior
 e o menor valores lidos.
 */
 console.clear();
+
+const teclado = require (`prompt-sync`)();
+let array1: number [] = new Array(9);
+
+for(let x =0; x <10; x++){
+    array1[x] = parseFloat(teclado(`Insira os numeros ${x + 1}°:  `));
+}
+
+let maior: number = 0;
+let menor: number = 0;
+
+for(let i = 0; i <10; i++){
+    if(array1[i] > maior){
+        maior = array1[i];
+        }
+}
+
+for(let i = 0; i <10; i++){
+    if(array1[i] < menor){
+        menor = array1[i];
+    }
+}
+
+console.log(`============================`);
+console.log(`|O Maior numero é: ${maior}|`);
+console.log(`|O Menor numero é: ${menor}|`);
+console.log(`============================`);
